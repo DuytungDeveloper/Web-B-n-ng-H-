9caaef34afcafaba8b967c80e5b9b591a973add0
trannghiaqn76 <trannghiaqn76@gmail.com>
2020-07-09 20:54:03 +0700

    nghia sửa lại các ràng buộc chung của dữ liệu database

--- ECommerce.Model/EFModel/Models/District.cs
+++ ECommerce.Model/EFModel/Models/District.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ECommerce.Model.EFModel.Models
 {
     public partial class District : BaseModel, IBaseModel
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public int? CityId { get; set; }
-        public int? Status { get; set; }
+        public int CityId { get; set; }
+        public virtual City City { get; set; }
+        public int Status { get; set; }
     }
 }
--- ECommerce.Model/EFModel/Models/Images.cs
+++ ECommerce.Model/EFModel/Models/Images.cs
@@ -9,7 +9,7 @@ namespace ECommerce.Model.EFModel.Models
         [Required]
         public int Id { get; set; }
      
-        public int? IdProduct { get; set; }
+        public int IdProduct { get; set; }
         public virtual Product Product { get; set; }
         [Required]
         public string Name { get; set; }
