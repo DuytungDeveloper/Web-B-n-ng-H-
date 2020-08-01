3db9145d9986d881876df6fb980f5b5654ce1b4c
Duy Tùng <duytung.developer@gmail.com>
2020-08-01 15:23:42 +0700

    Update Link

---
+++ ECommerce/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Controllers
+{
+    [Route("{language}/don-hang/{action=index}")]
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ECommerce/Controllers/SearchController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Controllers
+{
+    [Route("{language}/search-result")]
+    public class SearchController : Controller
+    {
+        [HttpGet()]
+        public IActionResult Index([FromQuery(Name = "page")] string page ="")
+        {
+            return View();
+        }
+    }
+}
